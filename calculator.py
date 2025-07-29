c752839892cadf389c3965b8de3f2142dfae0147
racolas1 <nikolasmlima@gmail.com>
2025-07-29 10:21:51 -0400

    completed

--- calculator.py
+++ calculator.py
@@ -22,7 +22,7 @@ def hypotenuse(a, b):
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
 def mul(a, b):
@@ -33,7 +33,7 @@ def div(a, b):
         raise ZeroDivisionError
     return b / a
 
-def log(a, b):
+def logarithm(a, b):
     if b <= 0 or b == 1:
         raise ValueError("base must be positive and not equal to 1")
     if a <= 0:
